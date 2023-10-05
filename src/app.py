15e32fa365bfdcd9dc48eb8b47f80d7d5c5fdd0a
MatthewNguyenRAZR <matthewpdn@gmail.com>
2023-10-05 17:00:59 -0400

    More Testing, Organizing

--- src/app.py
+++ src/app.py
@@ -4,13 +4,17 @@ https://pypi.org/project/PyAutoGUI/
 https://github.com/drov0/python-imagesearch 
 """
 from os.path import exists
-
+from screeninfo import get_monitors
+from python_imagesearch.imagesearch import *
 import time
 import pyautogui
 import pydirectinput
-from screeninfo import get_monitors
-from python_imagesearch.imagesearch import *
+import operator_card
 
+from PIL import ImageGrab
+from functools import partial
+
+ImageGrab.grab = partial(ImageGrab.grab, all_screens=True)
 
 pyautogui.PAUSE = 0
 pydirectinput.PAUSE = 0
@@ -50,22 +54,45 @@ def pyautogui_ocr_test():
     # pyautogui.leftClick()
 
 
-def python_imagesearch(filepath):
-    pos = imagesearch(filepath)
+def python_imagesearch(filepath, precision):
+    pos = imagesearch(filepath, precision)
     if pos[0] != -1:
-        print("position : ", pos[0], pos[1])
         x_pos = pos[0]
         y_pos = pos[1]
         if len(get_monitors()) > 1:
             x_pos = pos[0] - 1920
-        pyautogui.moveTo(x_pos, y_pos, duration=0.1)
+        print("Position : ", x_pos, y_pos)
+        return (x_pos, y_pos)
     else:
         print("image not found")
     # click_image("./newtab.jpg", pos, "right", 0.1, offset=5)
+    return None
+
+
+def screenshot_imagesearch():
+    img1 = pyautogui.screenshot(region=(-1893, 109, 500, 400))
+    # img1.save("../testimages/screenshot.png")
+    # print(pyautogui.position())
+
+
+def chikamaintest():
+    img_paths = [
+        "../resources/e2.png",
+        "../testimages/yt.jpg",
+        "../testimages/SariaOp.jpg",
+    ]
+    pos = python_imagesearch(img_paths[2], 0.8)
+    if pos != None:
+        pyautogui.moveTo(pos[0], pos[1], duration=0.1)
+        print("Mouse Moved")
+    screenshot_imagesearch()
+
+
+def meltymaintest():
+    print(operator_card.detect_elite())
 
 
 if __name__ == "__main__":
     print("Hello World")
-    path = "../resources/e2.png"
-    # path = "../testimages/yt.jpg"
-    python_imagesearch(path)
+    # meltymaintest()
+    # chikamaintest()
--- src/operator_card.py
+++ src/operator_card.py
@@ -3,15 +3,13 @@ from python_imagesearch.imagesearch import *
 
 
 def detect_elite():
-    elite = None
+    elite_pos = None
     for i in range(3):
         # how do we get it to recognize image that's not identical?
         # lowering precision will also give the wrong result
         pos = imagesearch(f"../resources/e{i}.png", 0.1)
+        pos = pos + (i,)
         if pos[0] != -1:
             # pyautogui.moveTo(pos[0], pos[1], duration=0.1)
-            elite = i
-    return elite
-
-
-print(detect_elite())
+            elite_pos = pos
+    return elite_pos
